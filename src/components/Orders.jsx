701fd7061faedce14994931a502e1026b7691d4b
bosarn <58468105+bosarn@users.noreply.github.com>
2020-06-21 21:51:34 +0200

    Major styling done -Snackbar fully up and running -fixed various major issues -orders finished -Everything mobiled -Various minor issues and errors fixed

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -16,7 +16,29 @@ useEffect(() => {
     dispatch(getOrders());
   }, [dispatch]);
 
-console.log(data)
+
+  const useStyles = makeStyles({
+    Order: {
+        marginTop: "3rem",
+        marginLeft: "3em",
+        width: '40%'
+    },
+    panel: {
+        borderTop: "20px solid pink",
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+      },
+      test: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      },
+  });
+
+  const classes = useStyles();
+
 return (
 
     <> 
